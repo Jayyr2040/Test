7d28551e1cf7088df84b64f49d0065c3a11dd8e8
jaytai2040 <jayyr2040@gmail.com>
2021-07-02 14:26:19 +0800

    Test7

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import "./App.css";
 import React from "react";
 import Moment from "react-moment";
 
-let globalCount = 0;
-
+let isFirstLoad = true;
 function App() {
   const [highlights, setHighlights] = React.useState([]);
   const [counter, setCounter] = React.useState(0);
   const urlRegex = /(https?:\/\/[^ ]*)/;
   const url2 = "https://www.scorebat.com/video-api/v1/";
 
-  if (globalCount === 0) {
-    console.log("Global Count - ",globalCount);
-    globalCount += 1;
+  if (isFirstLoad === true) {
+    console.log("First Load - ",isFirstLoad);
+    isFirstLoad = false;
 
     const makeAPICall1 = async () => {
       const response = await fetch(url2);
       if (!response.ok) {
         console.log("server bad response");
-        throw new Error("server bad response");
       }
       const data = await response.json();
       console.log("url2 - data", data);
@@ -27,9 +25,7 @@ function App() {
     makeAPICall1().catch(() => {
       console.log("There has been a problem with your fetch operation: ");
     });
-
   }
-
   const handleNext = () => {
     setCounter((prevCount) =>
       prevCount === highlights.length ? prevCount : prevCount + 1
@@ -40,7 +36,6 @@ function App() {
     setCounter((prevCount) => (prevCount === 0 ? prevCount : prevCount - 1));
   };
 
-  
   // const url1 = "https://gnews.io/api/v4/search?q="+"manchester"+" united"+ "&token=c276e35eade36eea9a97d3bb9c8eda9f&lang=en";
   // const url1 = "https://gnews.io/api/v4/search?q=manchester+united&token=bbfefcdb33b0c247e627cbc6cf6552a4&lang=en&max=2&in=title";
   //const url1 = "https://gnews.io/api/v4/search?q=manchester+united&token=c276e35eade36eea9a97d3bb9c8eda9f&lang=en&max=2&in=title";
@@ -55,50 +50,29 @@ function App() {
   //const url3 = "https://gnews.io/api/v4/top-headlines?q=manchester+united&token=3cdf1b72b7469b7836cc6c60746e5a40&lang=en&max=1&in=title";
   // const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos,images&language=en`;
   
-
-/*   React.useEffect(() => {
-    const makeAPICall1 = async () => {
-      const response = await fetch(url2);
-      if (!response.ok) {
-        console.log("server bad response");
-        throw new Error("server bad response");
-      }
-      const data = await response.json();
-      console.log("url2 - data", data);
-      setHighlights(data);
-    };
-    makeAPICall1().catch(() => {
-      console.log("There has been a problem with your fetch operation: ");
-    });
-    //    function comes here
-  }, [highlights]); */
-
   return (
     <div className="App">
       <h1>Headline news</h1>
       {/* {jokeArr?.articles?.[0]?.title}
- <br></br>
-  <Moment fromNow >{jokeArr?.articles?.[0]?.publishedAt}</Moment>
-  {jokeArr?.articles?.[0]?.source.name}
-  {jokeArr?.articles?.[0]?.url}
-  <img src={jokeArr?.articles?.[0]?.image} alt=""/>
+          <br></br>
+            <Moment fromNow >{jokeArr?.articles?.[0]?.publishedAt}</Moment>
+            {jokeArr?.articles?.[0]?.source.name}
+            {jokeArr?.articles?.[0]?.url}
+            <img src={jokeArr?.articles?.[0]?.image} alt=""/>
 
-  <h1>Top Headline news</h1>
-   {jokeArr3?.articles?.[0]?.title}
-   <br></br>
-    <Moment fromNow >{jokeArr3?.articles?.[0]?.publishedAt}</Moment>
-    {jokeArr3?.articles?.[0]?.source.name}
-     {jokeArr3?.articles?.[0]?.url}
-   <img src={jokeArr3?.articles?.[0]?.image} alt=""/> */}
-
-      {/* {(jokeArr2?.[73]?.embed)} */}
-
+            <h1>Top Headline news</h1>
+            {jokeArr3?.articles?.[0]?.title}
+            <br></br>
+              <Moment fromNow >{jokeArr3?.articles?.[0]?.publishedAt}</Moment>
+              {jokeArr3?.articles?.[0]?.source.name}
+              {jokeArr3?.articles?.[0]?.url}
+            <img src={jokeArr3?.articles?.[0]?.image} alt=""/> */}
       <div>
         <h1>Football highlights</h1>
       </div>
       <div>
-        <Moment fromNow>{highlights?.[counter]?.date}</Moment>
-        <p>{highlights?.[counter]?.competition.name}</p>
+        <Moment fromNow>{ highlights?.length > 0 ? highlights?.[counter]?.date : (isFirstLoad = true)}</Moment>
+        <p>{highlights?.[counter]?.competition?.name}</p>
         <p>{highlights?.[counter]?.title}</p>
         <button onClick={handlePrev}>Previous</button>
         <button onClick={handleNext}>Next</button>
@@ -109,7 +83,6 @@ function App() {
             width="560"
             height="650"
             alt=""
-            allowFullScreen
             allow="autoplay; fullscreen"
             title="This is a unique title"
           ></iframe>
